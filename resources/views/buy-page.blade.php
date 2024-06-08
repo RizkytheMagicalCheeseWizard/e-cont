6325aabae2f3b4437193eabbc157731e931431b3
RizkyMaulanaS <rizkyzky345@gmail.com>
2024-06-08 17:32:36 +0700

    landing

---
+++ resources/views/buy-page.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buy</title>
+    <link href="{{asset('css/buy.css')}}" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <nav>
+        <div class="landing-title">Ticket Online</div>
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="">Buy</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="">Log Out</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -18,46 +18,56 @@
     </nav>
     <div id="about">About</div>
     <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci illum nulla rem debitis doloremque distinctio perspiciatis totam cupiditate molestiae, dolorum quibusdam tenetur quo impedit odio suscipit delectus placeat! Itaque, debitis.</article>
-    <a href="/about-page">-></a>
+    <a href="/about-page">ABOUT</a>
     <div id="buy">Buy</div>
     <article>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis incidunt alias vitae reprehenderit asperiores quibusdam consequuntur, eveniet dolorum, quas nihil illo repudiandae ipsum aliquid corporis voluptatum iusto excepturi voluptate ab?</article>
-    <a href="/buy-page">-></a>
+    <a href="/buy-page">BUY</a>
     <footer>
       <div id="contact">Contact</div>
         <section>
           <ul role="list" class="divide-y divide-gray-100">
             <li class="user-item">
               <div class="user-info">
-                <img class="user-avatar" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80">
+                <img class="user-avatar">
                 <div class="user-details">
-                  <p class="user-name">Leslie Alexander</p>
-                  <p class="user-email">leslie.alexander@example.com</p>
+                  <p class="user-name">Pandu</p>
+                  <p class="user-email">pandu@gmail.com</p>
                 </div>
               </div>
               <div class="user-status">
                 <p class="user-title">Co-Founder / CEO</p>
-                <p class="last-seen">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
               </div>
             </li>
           </ul>
           <ul role="list" class="divide-y divide-gray-100">
             <li class="user-item">
               <div class="user-info">
-                <img class="user-avatar" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80">
+                <img class="user-avatar">
                 <div class="user-details">
-                  <p class="user-name">Leslie Alexander</p>
-                  <p class="user-email">leslie.alexander@example.com</p>
+                  <p class="user-name">Rizky</p>
+                  <p class="user-email">rizky@gmail.com</p>
                 </div>
               </div>
               <div class="user-status">
                 <p class="user-title">Co-Founder / CEO</p>
-                <p class="last-seen">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
               </div>
             </li>
           </ul>
-      </section>
+          <ul role="list" class="divide-y divide-gray-100">
+            <li class="user-item">
+              <div class="user-info">
+                <img class="user-avatar">
+                <div class="user-details">
+                  <p class="user-name">Sulthon</p>
+                  <p class="user-email">Sulthon@gmail.com</p>
+                </div>
+              </div>
+              <div class="user-status">
+                <p class="user-title">Co-Founder /  CEO</p>
+              </div>
+            </li>
+          </ul>
+        </section>
     </footer>
-    
-
 </body>
 </html>

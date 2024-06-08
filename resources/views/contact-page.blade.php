6f477691f92d07511fcf86fc04808e45501d7f6e
PappaIkan <pappaUnion@gmail.com>
2024-06-08 10:09:28 +0700

    contact-page

---
+++ resources/views/contact-page.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/contact.css')}}">
+    <title>Document</title>
+</head>
+<body>
+
+    <ul role="list" class="divide-y divide-gray-100">
+        <li class="user-item">
+          <div class="user-info">
+            <img class="user-avatar" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80">
+            <div class="user-details">
+              <p class="user-name">Leslie Alexander</p>
+              <p class="user-email">leslie.alexander@example.com</p>
+            </div>
+          </div>
+          <div class="user-status">
+            <p class="user-title">Co-Founder / CEO</p>
+            <p class="last-seen">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
+          </div>
+        </li>
+    </ul>
+    <ul role="list" class="divide-y divide-gray-100">
+        <li class="user-item">
+          <div class="user-info">
+            <img class="user-avatar" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80">
+            <div class="user-details">
+              <p class="user-name">Leslie Alexander</p>
+              <p class="user-email">leslie.alexander@example.com</p>
+            </div>
+          </div>
+          <div class="user-status">
+            <p class="user-title">Co-Founder / CEO</p>
+            <p class="last-seen">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>
+          </div>
+        </li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',function(){
     return view('about-page');
 });
+Route::get('/contact-page',function(){
+    return view('contact-page');
+});

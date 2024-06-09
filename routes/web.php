e90f3b7406d934a209af9dcac2ecdc99578e5edd
RizkyMaulanaS <rizkyzky345@gmail.com>
2024-06-09 23:45:47 +0700

    Active Route

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,19 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/register','auth.register')->name('register');
-Route::post('/register',[AuthController::class],'register');
+// Route::view('/register','auth.register')->name('register');
+// Route::post('/register',[AuthController::class],'register');
 
-Route::view('/login','auth.login')->name('login');
-Route::post('/login',[AuthController::class, 'login']);
+// Route::view('/login','auth.login')->name('login');
+// Route::post('/login',[AuthController::class, 'login']);
 
 
-// Route::get('/',function(){
-//     return view('landing-page');
-// });
-// Route::get('/about-page',function(){
-//     return view('about-page');
-// });
-// Route::get('/buy-page',function(){
-//     return view('buy-page');
-// });
+Route::get('/',function(){
+    return view('landing-page');
+});
+Route::get('/about-page',function(){
+    return view('about-page');
+});
+Route::get('/buy-page',function(){
+    return view('buy-page');
+});

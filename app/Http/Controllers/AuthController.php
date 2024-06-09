ee947df59f7468e40057ba7eeb258e1f9cd849d4
RizkyMaulanaS <rizkyzky345@gmail.com>
2024-06-09 23:45:02 +0700

    Del Login and Make Again

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,15 @@ class AuthController extends Controller
 {
     public function login(Request $request){
         $request->validate([
-            
+            'email' => ['required', 'max:255'],
+            'password' => ['required','min:3']
+        ]);
+    }
+    public function register(Request $request){
+        $request->validate([
+            'name' => ['required','max:255'],
+            'email' => ['required','max:255','name'],
+            'password' => ['required','min:3','confirmed']
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/register','auth.register')->name('register');
+Route::post('/register',[AuthController::class],'register');
 
 Route::view('/login','auth.login')->name('login');
 Route::post('/login',[AuthController::class, 'login']);

99b007a849c1cc897925890b5412880a26b6310d
RizkyMaulanaS <rizkyzky345@gmail.com>
2024-06-10 00:49:58 +0700

    fixing validate and seperate login and register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{env('APP_NAME')}}</title>
+  <link href="{{asset('css/auth.css')}}" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div class="wrapper">
+    <div class="card-switch">
+        <div class="flip-card__inner">
+          <div class="flip-card__front">
+            <div class="title">Sign up</div>
+            <form class="flip-card__form" action="{{route('register')}}" method="POST">
+              @csrf
+              <input class="flip-card__input" placeholder="Name" type="name">
+              @error('name')
+                  <p class="error-message">{{$message}}</p>
+              @enderror
+              <input class="flip-card__input" name="email" placeholder="Email" type="email">
+              @error('email')
+                  <p class="error-message">{{$message}}</p>
+              @enderror
+              <input class="flip-card__input" name="password" placeholder="Password" type="password">
+              @error('password')
+                  <p class="error-message">{{$message}}</p>
+              @enderror
+              <button class="flip-card__btn">Confirm!</button>
+            </form>
+          </div>
+          {{-- <div class="flip-card__back">
+            <div class="title">Sign up</div>
+            <form class="flip-card__form" action="{{route('register')}}" method="POST">
+              @csrf
+              <input class="flip-card__input" placeholder="Name" type="name">
+              @error('name')
+                  <p class="error-message">{{$message}}</p>
+              @enderror
+              <input class="flip-card__input" name="email" placeholder="Email" type="email">
+              @error('email')
+                  <p class="error-message">{{$message}}</p>
+              @enderror
+              <input class="flip-card__input" name="password" placeholder="Password" type="password">
+              @error('password')
+                  <p class="error-message">{{$message}}</p>
+              @enderror
+              <button class="flip-card__btn">Confirm!</button>
+            </form>
+          </div>
+        </div> --}}
+    </div>
+  </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Route::view('/register','auth.register')->name('register');
-// Route::post('/register',[AuthController::class],'register');
 
-// Route::view('/login','auth.login')->name('login');
-// Route::post('/login',[AuthController::class, 'login']);
 
+
+
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class, 'register']);
+Route::view('/login','auth.login')->name('login');
+Route::view('/register','auth.register')->name('register');
 
 Route::get('/',function(){
     return view('landing-page');

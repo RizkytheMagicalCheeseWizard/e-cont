d39e73cd0fff270f574e2d9b30f1c736faf7acbc
RizkyMaulanaS <rizkyzky345@gmail.com>
2024-07-01 17:01:10 +0700

    ngide lagi

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -11,59 +11,25 @@
     <div class="card-about">
       <div id="about">About Us</div>
       <article class="article-about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci illum nulla rem debitis doloremque distinctio perspiciatis totam cupiditate molestiae, dolorum quibusdam tenetur quo impedit odio suscipit delectus placeat! Itaque, debitis.</article>
-      <a href={{route('about')}}>ABOUT</a>
+      <img class="img-arrow" src="{{asset('image/arow-down.png')}}"> 
+      <div><a class="to-about" href={{route('about')}}>ABOUT</a></div>
     </div>
     <div class="card-buy">
       <div id="buy">Buy</div>
       <article class="article-buy">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis incidunt alias vitae reprehenderit asperiores quibusdam consequuntur, eveniet dolorum, quas nihil illo repudiandae ipsum aliquid corporis voluptatum iusto excepturi voluptate ab?</article>
-      <a class="buy-btn" href={{route('buy')}}><img class="bus-icon" src="{{asset('image/bus.png')}}"></a>  
-    </div> 
-    
-    <div class="card-contact"></div>
-      <div id="contact">Contact</div>
-        <section>
-          <ul role="list" class="divide-y divide-gray-100">
-            <li class="user-item">
-              <div class="user-info">
-                <img src={{asset('image/p.png') }} class="user-avatar">
-                <div class="user-details">
-                  <p class="user-name">Pandu</p>
-                  <p class="user-email">pandu@gmail.com</p>
-                </div>
-              </div>
-              <div class="user-status">
-                <p class="user-title">MIAW</p>
-              </div>
-            </li>
-          </ul>
-          <ul role="list" class="divide-y divide-gray-100">
-            <li class="user-item">
-              <div class="user-info">
-                <img src={{asset('image/r.png')}} class="user-avatar">
-                <div class="user-details">
-                  <p class="user-name">Rizky</p>
-                  <p class="user-email">rizky@gmail.com</p>
-                </div>
-              </div>
-              <div class="user-status">
-                <p class="user-title">MIAW</p>
-              </div>
-            </li>
-          </ul>
-          <ul role="list" class="divide-y divide-gray-100">
-            <li class="user-item">
-              <div class="user-info">
-                <img src={{asset('image/s.png')}} class="user-avatar">
-                <div class="user-details">
-                  <p class="user-name">Sulthon</p>
-                  <p class="user-email">sulthon@gmail.com</p>
-                </div>
-              </div>
-              <div class="user-status">
-                <p class="user-title">MIAW</p>
-              </div>
-            </li>
-          </ul>
-        </section>
+      <img class="img-arrow" src="{{asset('image/arow-down.png')}}">
+      <div><a class="buy-btn" href={{route('buy')}}><img class="bus-icon" src="{{asset('image/bus.png')}}"></a></div>  
+    </div>
+    <footer>
+      <div class="left-side">
+        <div class="">E-Cont</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur sunt unde consequatur dolor amet, nostrum, fugit ex, repellat expedita dolores consequuntur necessitatibus ut quia. Cupiditate voluptatem odit tenetur amet at.</div>
+      </div>
+      <div class="right-side">
+        <div class=""></div>
+      </div>
+      
+      
+    </footer> 
 </body>
 </html>

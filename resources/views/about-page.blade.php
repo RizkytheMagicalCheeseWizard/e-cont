39fa2e236010306cc7074f223687a1c338ab649e
PappaIkan <pappaUnion@gmail.com>
2024-07-05 07:14:46 +0700

    about-fix

--- resources/views/about-page.blade.php
+++ resources/views/about-page.blade.php
@@ -1,41 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{env('APP_NAME')}}</title>
-    <link href="{{asset('css/about.css')}}" rel="stylesheet" type="text/css">
+    <title>{{ env('APP_NAME') }}</title>
+    <link href="{{ asset('css/about.css') }}" rel="stylesheet" type="text/css">
 </head>
+
 <body>
     <x-navbar></x-navbar>
     <div class="container">
-    <div class="flex-to-center">
-        <div class="wrapper-gradient-border-radius">
-            <div class="img"><img></div>
-            <div class="name">Pandu Adi</div>
-            <div class="social-transition">
-                <div><a href=""><img>@Pandu</a></div>
-                <div><a href=""><img>PappaIkan</a></div>
+        <div class="flex-to-center">
+            <div class="wrapper-gradient-border-radius">
+                <div><img class="img" src="{{ asset('image/pan.jpg') }}"></div>
+                <div class="name">Pandu Adi</div>
+                <div class="social-transition">
+                    <div><a href="https://www.instagram.com/panduakw_/"><img>@Pandu</a></div>
+                    <div><a href="https://github.com/PappaIkan"><img>PappaIkan</a></div>
+                </div>
+            </div>
+            <div class="wrapper-gradient-border-radius">
+                <div><img class="img" src="{{ asset('image/rizk.jpg') }}"></div>
+                <div class="name">Rizky Maulana</div>
+                <div class="social-transition">
+                    <div><a href="https://www.instagram.com/rzky._mauln/"><img>@Rizky</a></div>
+                    <div><a href="https://github.com/RizkytheMagicalCheeseWizard"><img>RizkyTheMagicalCheeseWizard</a>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper-gradient-border-radius">
+                <div><img class="img" src="{{ asset('image/shul.jpg') }}"></div>
+                <div class="name">Sulthon Kaffah</div>
+                <div class="social-transition">
+                    <div><a href="https://www.instagram.com/sulthonkaf/"><img>@Sulthon</a></div>
+                    <div><a href="https://github.com/MenDev17"><img>Sulthon</a></div>
+                </div>
             </div>
         </div>
-        <div class="wrapper-gradient-border-radius">
-            <div class="img"><img></div>
-            <div class="name">Rizky Maulana</div>
-            <div class="social-transition">
-                <div><a href=""><img>@Rizky</a></div>
-                <div><a href=""><img>RizkyTheMagicalCheeseWizard</a></div>
-            </div>
-        </div>
-        <div class="wrapper-gradient-border-radius">
-            <div class="img"><img></div>
-            <div class="name">Sulthon Kaffah</div>
-            <div class="social-transition">
-                <div><a href=""><img>@Sulthon</a></div>
-                <div><a href=""><img src="" alt="">Sulthon</a></div>
-            </div>
-        </div>
-    </div>
     </div>
     <x-footer></x-footer>
 </body>
+
 </html>
